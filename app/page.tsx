efbce97d66ed14c71c752756caf0e0215020e27b
kyle.lan <kylel@nfcc.cc>
2025-04-25 21:02:24 +0800

    feat: Reset polygon state on dialog close to improve user experience

--- app/page.tsx
+++ app/page.tsx
@@ -732,6 +732,8 @@ export default function Home() {
                                     setShowPolygonDialog(false)
                                     setNewPolygonName("")
                                     setNameError("")
+                                    setPolygonPoints([])
+                                    setIsDrawingPolygon(false)
                                 }}
                                 className="rounded bg-gray-500 px-4 py-2 text-white"
                             >
